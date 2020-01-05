88f5fdad9ac73c70699ba1f0bbf0a335932b66a8
Srevin Saju <srevin03@gmail.com>
2020-01-06 02:19:39 +0300

    Port to Gtk3, Python3 and port to sugargame [GCI] (#3)
    * remove pyc files
    
    * Fix setup.py to Python3 Gtk3
    
    * Replace all shebangs to python3
    
    * Fix pep8
    
    * Port to Python3 (2to3)
    
    * Add Sugargame package
    
    * Port to sugar3 activity.info and fix deprecation
    
    * Port to GTK3
    
    * Remove redundant comments, blank lines
    
    * Add .gitignore
    
    * Pep8 and flake8 fixes for root files
    
    * Pep8 and flake8 fixes for JAM source files
    
    * Make setup.py executable
    
    * Add Stop Button to Toolbar Activity, and refactor build_toolbar

--- BiblioJAM/JAMFire.py
+++ BiblioJAM/JAMFire.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 #   BiblioJAM (Versión 2.0) - 31/05/2011 - CeibalJAM! - Uruguay
@@ -7,150 +7,161 @@
 #   https://sites.google.com/site/sugaractivities/
 #   http://codigosdeejemplo.blogspot.com/
 
-import pygame, gc, sys, os
+from . import JAMGlobals as VG
+import pygame
+import gc
+import sys
+import os
 from pygame.locals import *
 gc.enable()
 
-import JAMGlobals as VG
 
 class JAMFire(pygame.sprite.OrderedUpdates):
-	''' Efecto Fuego. '''
-	def __init__(self):
-		pygame.sprite.OrderedUpdates.__init__(self)
-		self.posicion= (0,0)
-		self.latencia= 6
-		self.disminucion= 5
-		self.deformacion= (0,0)
+    ''' Efecto Fuego. '''
 
-		self.fuego= Fuego(self) 
-		self.add(self.fuego)
+    def __init__(self):
+        pygame.sprite.OrderedUpdates.__init__(self)
+        self.posicion = (0, 0)
+        self.latencia = 6
+        self.disminucion = 5
+        self.deformacion = (0, 0)
 
-	def set_posicion(self, punto= (0,0)):
-		''' Setea la posición desde donde se dibujará el efecto, es decir que punto es la base del fuego.'''
-		if type(punto)== tuple:
-			if len(punto)== 2 and type(punto[0])==int and type(punto[1]):
-				self.fuego.rect.x= punto[0]-self.fuego.rect.w/2
-				self.fuego.rect.y= punto[1]-self.fuego.rect.h
-				self.posicion= punto
+        self.fuego = Fuego(self)
+        self.add(self.fuego)
 
-	def set_deforma_fire(self, valor):
-		''' Divide por valor el tamaño del fuego. '''
-		if type(valor)== tuple:
-			if len(valor)==2 and type(valor[0])== int and type(valor[1])== int:
-				self.deformacion= valor
-				self.fuego.deforma_imagenes()
-				self.set_posicion(punto= self.posicion)
+    def set_posicion(self, punto=(0, 0)):
+        ''' Setea la posición desde donde se dibujará el efecto, es decir que punto es la base del fuego.'''
+        if type(punto) == tuple:
+            if len(punto) == 2 and type(punto[0]) == int and type(punto[1]):
+                self.fuego.rect.x = punto[0]-self.fuego.rect.w/2
+                self.fuego.rect.y = punto[1]-self.fuego.rect.h
+                self.posicion = punto
 
-	def set_disminuye_fire(self, valor):
-		''' Divide por valor el tamaño del fuego. '''
-		if type(valor)== int and valor > 0:
-			self.disminucion= valor
-			self.fuego.load_imagenes()
-			self.set_posicion(punto= self.posicion)
+    def set_deforma_fire(self, valor):
+        ''' Divide por valor el tamaño del fuego. '''
+        if type(valor) == tuple:
+            if len(valor) == 2 and type(valor[0]) == int and type(valor[1]) == int:
+                self.deformacion = valor
+                self.fuego.deforma_imagenes()
+                self.set_posicion(punto=self.posicion)
 
-	def set_latencia(self, latencia):
-		''' Setea la velocidad de secuenciación de imágenes para el fuego. '''
-		if type(latencia)== int: self.latencia= latencia
+    def set_disminuye_fire(self, valor):
+        ''' Divide por valor el tamaño del fuego. '''
+        if type(valor) == int and valor > 0:
+            self.disminucion = valor
+            self.fuego.load_imagenes()
+            self.set_posicion(punto=self.posicion)
 
-	def get_tamanio(self):
-		''' Devuelve el tamanio actual del fuego. '''
-		return (self.fuego.rect.w, self.fuego.rect.h)
+    def set_latencia(self, latencia):
+        ''' Setea la velocidad de secuenciación de imágenes para el fuego. '''
+        if type(latencia) == int:
+            self.latencia = latencia
+
+    def get_tamanio(self):
+        ''' Devuelve el tamanio actual del fuego. '''
+        return (self.fuego.rect.w, self.fuego.rect.h)
+
 
 class Fuego(pygame.sprite.Sprite):
-	''' El Sprite para el efecto. '''
-	def __init__(self, fire):
-		pygame.sprite.Sprite.__init__(self)
-		self.fire= fire
-		self.imagenes= []
-		self.indice_actual= 0
-		self.image= None
-		self.rect= None
-		self.contador= 0
-		self.load_imagenes()
+    ''' El Sprite para el efecto. '''
 
-	def load_imagenes(self):
-		''' carga las imagenes ajustando su tamaño según el indice de disminución especificado. '''
-		self.imagenes= []
-		for imagen in VG.get_fire():
-			tam= imagen.get_size()
-			im= pygame.transform.scale(imagen, (tam[0]/self.fire.disminucion,tam[1]/self.fire.disminucion)) 
-			self.imagenes.append(im)
-		self.indice_actual= 0
-		self.image= self.imagenes[self.indice_actual]
-		self.rect= self.image.get_rect()
-		self.contador= 0
+    def __init__(self, fire):
+        pygame.sprite.Sprite.__init__(self)
+        self.fire = fire
+        self.imagenes = []
+        self.indice_actual = 0
+        self.image = None
+        self.rect = None
+        self.contador = 0
+        self.load_imagenes()
 
-	def deforma_imagenes(self):
-		''' carga las imagenes ajustando su tamaño según la información en self.fire.deformacion. '''
-		self.imagenes= []
-		for imagen in VG.get_fire():
-			im= pygame.transform.scale(imagen, self.fire.deformacion) 
-			self.imagenes.append(im)
-		self.indice_actual= 0
-		self.image= self.imagenes[self.indice_actual]
-		self.rect= self.image.get_rect()
-		self.contador= 0
-			
-	def update(self):
-		''' Secuencia las imágenes. '''
-		if self.contador == self.fire.latencia:
-			try:
-				self.indice_actual += 1 
-				self.image= self.imagenes[self.indice_actual]
-			except:
-				self.indice_actual = 0
-				self.image= self.imagenes[self.indice_actual]
-			self.contador = 0
-		self.contador += 1
+    def load_imagenes(self):
+        ''' carga las imagenes ajustando su tamaño según el indice de disminución especificado. '''
+        self.imagenes = []
+        for imagen in VG.get_fire():
+            tam = imagen.get_size()
+            im = pygame.transform.scale(
+                imagen, (tam[0]/self.fire.disminucion, tam[1]/self.fire.disminucion))
+            self.imagenes.append(im)
+        self.indice_actual = 0
+        self.image = self.imagenes[self.indice_actual]
+        self.rect = self.image.get_rect()
+        self.contador = 0
+
+    def deforma_imagenes(self):
+        ''' carga las imagenes ajustando su tamaño según la información en self.fire.deformacion. '''
+        self.imagenes = []
+        for imagen in VG.get_fire():
+            im = pygame.transform.scale(imagen, self.fire.deformacion)
+            self.imagenes.append(im)
+        self.indice_actual = 0
+        self.image = self.imagenes[self.indice_actual]
+        self.rect = self.image.get_rect()
+        self.contador = 0
+
+    def update(self):
+        ''' Secuencia las imágenes. '''
+        if self.contador == self.fire.latencia:
+            try:
+                self.indice_actual += 1
+                self.image = self.imagenes[self.indice_actual]
+            except:
+                self.indice_actual = 0
+                self.image = self.imagenes[self.indice_actual]
+            self.contador = 0
+        self.contador += 1
 
 # ----- FIN DE CLASE JAMFire - INICIO DE DEBUG Y EJEMPLO DE LA CLASE -----
+
+
 class Ejemplo(object):
-	def __init__(self):
-		self.ventana = None
-		self.reloj = None
-		self.nivel = "menu_0"
+    def __init__(self):
+        self.ventana = None
+        self.reloj = None
+        self.nivel = "menu_0"
 
-		self.fondo = None
-		self.widgets = None
+        self.fondo = None
+        self.widgets = None
 
-		self.resolucion = (800,600)
+        self.resolucion = (800, 600)
 
-		self.setup()
-		self.Run()
+        self.setup()
+        self.Run()
 
-	def setup(self):
-		pygame.init()
-		pygame.display.set_mode(self.resolucion , 0, 0)
-		pygame.display.set_caption("Ejemplo de Efecto JAMFire")
-		self.fondo = self.get_Fondo()
+    def setup(self):
+        pygame.init()
+        pygame.display.set_mode(self.resolucion, 0, 0)
+        pygame.display.set_caption("Ejemplo de Efecto JAMFire")
+        self.fondo = self.get_Fondo()
 
-		#from JAMDragAndDrop import JAMDragAndDrop
-		#fire= JAMFire()
-		#self.widgets= JAMDragAndDrop(fire)
-		self.widgets = JAMFire()
-		self.widgets.set_posicion(punto= (400,300))
-		#self.widgets.set_disminuye_fire(1)
-		#self.widgets.set_deforma_fire((50,100))
-		self.ventana = pygame.display.get_surface()
-		self.reloj = pygame.time.Clock()
-		pygame.event.set_blocked([JOYAXISMOTION, JOYBALLMOTION, JOYHATMOTION, JOYBUTTONUP, JOYBUTTONDOWN,
-					USEREVENT, QUIT, ACTIVEEVENT])
-		pygame.event.set_allowed([MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN, KEYDOWN, KEYUP, VIDEORESIZE, VIDEOEXPOSE])
-		pygame.mouse.set_visible(True)
+        #from JAMDragAndDrop import JAMDragAndDrop
+        #fire= JAMFire()
+        #self.widgets= JAMDragAndDrop(fire)
+        self.widgets = JAMFire()
+        self.widgets.set_posicion(punto=(400, 300))
+        # self.widgets.set_disminuye_fire(1)
+        # self.widgets.set_deforma_fire((50,100))
+        self.ventana = pygame.display.get_surface()
+        self.reloj = pygame.time.Clock()
+        pygame.event.set_blocked([JOYAXISMOTION, JOYBALLMOTION, JOYHATMOTION, JOYBUTTONUP, JOYBUTTONDOWN,
+                                  USEREVENT, QUIT, ACTIVEEVENT])
+        pygame.event.set_allowed(
+            [MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN, KEYDOWN, KEYUP, VIDEORESIZE, VIDEOEXPOSE])
+        pygame.mouse.set_visible(True)
 
-	def Run(self):
-		self.ventana.blit(self.fondo, (0,0))
-		self.widgets.draw(self.ventana)
-		pygame.display.update()
+    def Run(self):
+        self.ventana.blit(self.fondo, (0, 0))
+        self.widgets.draw(self.ventana)
+        pygame.display.update()
 
-		contador= 0
-		while self.nivel == "menu_0":
-			self.reloj.tick(35)
-	
-			cambios=[]
-			self.widgets.clear(self.ventana, self.fondo)
+        contador = 0
+        while self.nivel == "menu_0":
+            self.reloj.tick(35)
 
-			'''
+            cambios = []
+            self.widgets.clear(self.ventana, self.fondo)
+
+            '''
 			if contador==100:
 				self.widgets.set_posicion(punto= (200,300))
 				self.widgets.set_disminuye_fire(2)
@@ -184,30 +195,31 @@ class Ejemplo(object):
 				contador= 0
 			contador += 1'''
 
-			self.widgets.update()
-			self.handle_event()
-			pygame.event.clear()
-			cambios.extend ( self.widgets.draw(self.ventana) )
-			pygame.display.update(cambios)
+            self.widgets.update()
+            self.handle_event()
+            pygame.event.clear()
+            cambios.extend(self.widgets.draw(self.ventana))
+            pygame.display.update(cambios)
 
-	def get_Fondo(self):
-		superficie = pygame.Surface( self.resolucion, flags=HWSURFACE )
-		superficie.fill(VG.get_gris1())
-		#superficie.fill(VG.get_negro())
-		#superficie.fill(VG.get_blanco())
-		return superficie
+    def get_Fondo(self):
+        superficie = pygame.Surface(self.resolucion, flags=HWSURFACE)
+        superficie.fill(VG.get_gris1())
+        # superficie.fill(VG.get_negro())
+        # superficie.fill(VG.get_blanco())
+        return superficie
 
-	def handle_event(self):
-		for event in pygame.event.get():
-			if event.type == pygame.KEYDOWN:
-				teclas = pygame.key.get_pressed()
-				if teclas[pygame.K_ESCAPE]:
-					self.salir()
-		pygame.event.clear()
+    def handle_event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                teclas = pygame.key.get_pressed()
+                if teclas[pygame.K_ESCAPE]:
+                    self.salir()
+        pygame.event.clear()
 
-	def salir(self):
-		pygame.quit()
-		sys.exit()
+    def salir(self):
+        pygame.quit()
+        sys.exit()
+
 
 if __name__ == "__main__":
-	Ejemplo()
+    Ejemplo()
--- JAMClock.py
+++ JAMClock.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 #   JAMClock.py por:
@@ -19,185 +19,110 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import os, gtk, pygtk, gobject, sys, socket, pygame
-
-from pygame.locals import *
-from sugar.activity import activity
-
 from Main import Main
+from sugar3.activity.widgets import ActivityToolbarButton
+from sugar3.activity.widgets import StopButton
+from sugar3.graphics.toolbarbox import ToolbarBox
+from sugar3.activity import activity
+from pygame.locals import *
+from sugargame import event
+from sugargame import canvas
+import sugargame
+from sugar3.graphics.style import GRID_CELL_SIZE
+import pygame
+import socket
+import sys
+from gi.repository import Gtk, GObject, Gdk
+import os
+import gi
+gi.require_version('Gtk', '3.0')
+
 
 class JAMClock(activity.Activity):
-	def __init__(self, handle):
-	        activity.Activity.__init__(self, handle, False)
-		self.set_title('JAMClock')
-	        #self.set_toolbox(activity.ActivityToolbox(self))
-		self.eventbox= PygameCanvas()
-		self.set_canvas(self.eventbox)
+    def __init__(self, handle):
+        activity.Activity.__init__(self, handle, False)
+        self.set_title('JAMClock')
+        # self.set_toolbox(activity.ActivityToolbox(self))
 
-		self.add_events(gtk.gdk.ALL_EVENTS_MASK)
-		self.connect("destroy", self.salir)
+        w = Gdk.Screen.width()
+        h = Gdk.Screen.height() - 1 * GRID_CELL_SIZE
 
-		self.show_all()
-		self.realize()
+        # Start the Game activity
+        self.game = Main((w, h))
 
-	       	os.putenv('SDL_WINDOWID', str(self.eventbox.socket.get_id()))
-		gobject.idle_add(self.get_run_game)
+        # Build the Pygame canvas and start the game running
+        # (self.game.run is called when the activity constructor
+        # returns).
+        self._pygamecanvas = canvas.PygameCanvas(
+            self, main=self.game.run, modules=[pygame.display])
 
-	def get_run_game(self):
-		print "Lanzando JAMClock."
-		pygame.init()
-		x, y, w, y=  self.eventbox.get_allocation()
-		Main( (w,y) )
-		return False
+        # Note that set_canvas implicitly calls read_file when
+        # resuming from the Journal.
 
-	def salir(self, widget):
-		lambda w: gtk.main_quit()
-		sys.exit()
+        self._pygamecanvas.set_size_request(w, h)
+        self.set_canvas(self._pygamecanvas)
+        self._pygamecanvas.grab_focus()
 
-class PygameCanvas(gtk.EventBox):
-	def __init__(self):
-		gtk.EventBox.__init__(self)
-		self.set_flags(gtk.CAN_FOCUS)
-		self.setup_events()
-		self.socket = gtk.Socket()
-		self.add(self.socket)
-		self.button_state = [0,0,0]
-        	self.mouse_pos = (0,0)
-		
-	def setup_events(self):
-		self.set_events(gtk.gdk.KEY_PRESS | gtk.gdk.EXPOSE | gtk.gdk.POINTER_MOTION_MASK | \
-		            gtk.gdk.POINTER_MOTION_HINT_MASK | gtk.gdk.BUTTON_MOTION_MASK | \
-		            gtk.gdk.BUTTON_PRESS_MASK | gtk.gdk.BUTTON_RELEASE_MASK)
-	
-		self.connect("key-press-event", self.keypress)
-		self.connect("button_press_event", self.mousedown)
-		self.connect("motion-notify-event", self.mousemotion)
-		self.connect('expose-event', self.expose)
-		self.connect('configure-event', self.resize)
-		self.connect("focus-in-event", self.set_focus)
+        # Build the activity toolbar.
+        self.build_toolbar()
 
-	def keypress(self, selfmain, event, parametros= None):
-		nombre= gtk.gdk.keyval_name(event.keyval)
-		tipo= pygame.KEYDOWN
-		unic= str.lower(nombre)
-		valor= nombre
-		try:
-			valor= getattr(pygame, "K_%s" % (str.upper(nombre)))
-		except:
-			print "no has programado la traduccion de esta tecla: ", nombre
-			return False
-		evt = pygame.event.Event(tipo, key= valor, unicode= unic, mod=None)
-		try:
-			pygame.event.post(evt)
-		except:
-			pass
-		return False
+    def build_toolbar(self):
+        self.toolbar_box = ToolbarBox()
+        self.activity_button = ActivityToolbarButton(self)
+        self.toolbar_box.toolbar.insert(self.activity_button, 0)
+        self.activity_button.show()
 
-	def mousedown(self, widget, event):
-		evt = pygame.event.Event(pygame.MOUSEBUTTONDOWN, button= event.button, pos=(int(event.x), int(event.y)))
-		try:
-			pygame.event.post(evt)
-		except:
-			pass
-		return False
+        self._separator = Gtk.SeparatorToolItem()
+        self._separator.props.draw = False
+        self._separator.set_expand(True)
+        self.toolbar_box.toolbar.insert(self._separator, -1)
+        self._separator.show()
 
-	def mousemotion(self, widget, event):
-		x, y, state = event.window.get_pointer()
-        	rel = (x - self.mouse_pos[0], y - self.mouse_pos[1])
-        	self.mouse_pos= (int(x), int(y))
-        	self.button_state = [
-            	state & gtk.gdk.BUTTON1_MASK and 1 or 0,
-            	state & gtk.gdk.BUTTON2_MASK and 1 or 0,
-            	state & gtk.gdk.BUTTON3_MASK and 1 or 0,
-        	]
-		evt = pygame.event.Event(pygame.MOUSEMOTION, pos= self.mouse_pos, rel=rel, buttons=self.button_state)
-		try:
-			pygame.event.post(evt)
-		except:
-			pass
-		return False
+        self.stop = StopButton(self)
+        self.toolbar_box.toolbar.insert(self.stop, -1)
+        self.stop.show()
+        
+        self.set_toolbar_box(self.toolbar_box)
+        self.toolbar_box.show_all()
+        return self.toolbar_box
 
-	def expose(self, event, widget):
-		if pygame.display.get_init():
-			try:
-				pygame.event.post(pygame.event.Event(pygame.VIDEOEXPOSE))
-			except:
-				pass
-		return False # continue processing
+    def get_run_game(self):
+        raise NotImplementedError
+        pass
 
-	def resize(self, widget, event):
-		evt = pygame.event.Event(pygame.VIDEORESIZE, size=(event.width,event.height), width=event.width, height=event.height)
-		try:
-			pygame.event.post(evt)
-		except:
-			pass
-		return False # continue processing
+    def salir(self, widget):
+        lambda w: Gtk.main_quit()
+        sys.exit()
 
-	def set_focus(self, container, widget):
-		try:
-			pygame.display.update()
-		except:
-			pass
-		self.queue_draw()
-		return False
 
 # -----------------------------------------------
-# 	******** El Juego en gtk ********
+# 	******** El Juego en Gtk ********
 # -----------------------------------------------
-class VentanaGTK(gtk.Window):
-	def __init__(self):
-		gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
-		self.set_title("JAMClock")
-		#self.fullscreen()
-		self.set_size_request(800,600)
-                self.socket = gtk.Socket()
-                self.add(self.socket)
 
-		self.gtkplug= gtkplug()
-		self.socket.add_id(self.gtkplug.get_id())	
-		
-		self.add_events(gtk.gdk.ALL_EVENTS_MASK)
-		self.connect("destroy", self.salir)
-		self.connect("set-focus-child", self.refresh)
-		self.show_all()
 
-	def refresh(self, widget, datos):
-		try:
-			pygame.display.update()
-		except:
-			pass
-		self.queue_draw()
-		return True
+class VentanaGTK(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, Gtk.WINDOW_TOPLEVEL)
+        self.set_title("JAMClock")
+        # self.fullscreen()
+        self.set_size_request(800, 600)
+        self.socket = Gtk.Socket()
+        self.add(self.socket)
+        self.ventana = None
 
-	def salir(self, widget):
-		pygame.quit()
-		sys.exit()
+        self.add_events(Gdk.ALL_EVENTS_MASK)
+        self.connect("destroy", self.salir)
+        self.connect("set-focus-child", self.refresh)
+        self.show_all()
 
-class gtkplug(gtk.Plug):
-	def __init__(self):
-		gtk.Plug.__init__(self, 0L)
-		self.set_title("JAMClock")
-		self.eventbox= PygameCanvas()
-		self.add(self.eventbox)
-		self.ventana= None
-		self.show_all()
+    def refresh(self, widget, datos):
+        try:
+            pygame.display.update()
+        except:
+            pass
+        self.queue_draw()
+        return True
 
-		self.connect("embedded", self.embed_event)
-
-	       	os.putenv('SDL_WINDOWID', str(self.eventbox.socket.get_id()))
-		gobject.idle_add(self.get_run_game)
-
-	def get_run_game(self):
-        	self.eventbox.socket.window.set_cursor(None)
-		print "Lanzando JAMClock."
-		pygame.init()
-		x, y, w, y=  self.eventbox.get_allocation()
-		Main( (w,y) )
-		return False
-
-	def embed_event(self, widget):
-	    	print "Juego embebido"
-
-if __name__=="__main__":
-	VentanaGTK()
-	gtk.main()
+    def salir(self, widget):
+        pygame.quit()
+        sys.exit()
